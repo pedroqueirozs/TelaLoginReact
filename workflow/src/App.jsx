7ebc9a68a41e9cb3c31816b729063ef5f10c9755
Pedro Queiroz <douglas.333.goncalves@gmail.com>
2023-02-27 23:07:39 -0300

    validando inputs

--- workflow/src/App.jsx
+++ workflow/src/App.jsx
@@ -1,21 +1,29 @@
+
 import logo from './assets/logo.svg';
 import './global.css'
-
+import { useForm } from "react-hook-form";
 
 export function App(){
+  const {register, handleSubmit, watch, formState: { errors } } = useForm();
+    function createUsers (data){
+      console.log(data);
+    } 
+
   return <div className="container">
     <header>
       <img src={logo} alt="Workflow" />
       <span>Merci d'entrer voss informations de connexion</span>
     </header>
-    <form id='form'>
+    <form onSubmit={handleSubmit(createUsers)} id='form'>
       <div className="inputContainer">
         <label htmlFor="e-mail">E-mail</label>
-        <input required type="e-mail" name="email" id="email" placeholder="pedro@gmail.com"/>
+        <input  type="email" name="email" id="email" placeholder="pedro@gmail.com"{...register("email",{required:true})}/>
+        {errors.email && <span>Insira seu e-mail < span className='obrigatorio'>*</span></span>}
       </div>
       <div className="inputContainer">
         <label htmlFor="password">Mot de passe</label>
-        <input minLength={8} type="password" name="password" id="password" placeholder="*********"/>
+        <input minLength={8} type="password" name="password" id="password" placeholder="*********"{...register("password",{required:true})}/>
+        {errors.password && <span>O campo de senha é obrigatório < span className='obrigatorio'>*</span></span>}
       </div>
       <a href="#" >Mot de passe oublié?</a>
       <button form='form' className="btn">Se connecter
@@ -28,3 +36,5 @@ export function App(){
     </form>
   </div>
 }
+
+
